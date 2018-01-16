92742d92432bf0d820e8bcd45329c0e23a0b1cc3
Dylan William Hardison <dylan@hardison.net>
2018-01-15 19:43:23 -0500

    use left join for requestee

--- Bugzilla/Model/Result/Flag.pm
+++ Bugzilla/Model/Result/Flag.pm
@@ -24,11 +24,10 @@ __PACKAGE__->add_columns(
 
 __PACKAGE__->set_primary_key(Bugzilla::Flag->ID_FIELD);
 
-__PACKAGE__->belongs_to(requestee => 'Bugzilla::Model::Result::User', 'requestee_id');
+__PACKAGE__->belongs_to( requestee => 'Bugzilla::Model::Result::User', 'requestee_id', { join_type => 'left' } );
 __PACKAGE__->belongs_to(requester => 'Bugzilla::Model::Result::User', 'setter_id');
 __PACKAGE__->belongs_to(bug => 'Bugzilla::Model::Result::Bug', 'bug_id');
 __PACKAGE__->has_one(type => 'Bugzilla::Model::Result::FlagType', 'id');
-
 __PACKAGE__->has_many('bug_group_map' => 'Bugzilla::Model::Result::BugGroupMap', 'bug_id');
 __PACKAGE__->belongs_to('attachment'  => 'Bugzilla::Model::Result::Attachment', 'attach_id', { join_type => 'left' });
 
